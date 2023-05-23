c38b850e8716810f26b98c069beeecfd35eea585
Kseniya_Oznobishina <Kseniya_Oznobishina@epam.com>
2023-05-23 18:56:30 +0300

    1 commit - create project

---
+++ src/main/java/com/example/pagenumbersreducer/PagenumbersreducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pagenumbersreducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagenumbersreducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagenumbersreducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pagenumbersreducer/PagenumbersreducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pagenumbersreducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagenumbersreducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
